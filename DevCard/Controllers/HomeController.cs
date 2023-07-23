2f9e84da5e035c36e1e712a71fbbf869deae3e60
neda-h <51288315+neda-h@users.noreply.github.com>
2023-07-23 12:17:02 +0430

    Index & Contact added

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -6,11 +6,9 @@ namespace DevCard.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -18,10 +16,11 @@ namespace DevCard.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
-            return View();
+	        return View(); 
         }
+    
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
