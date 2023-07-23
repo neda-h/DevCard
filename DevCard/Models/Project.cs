34856451fd3b5886588ba571e2ce3959875ceef1
neda-h <51288315+neda-h@users.noreply.github.com>
2023-07-23 14:10:05 +0430

    Projects And Articles moved to ViewComponent

---
+++ DevCard/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿namespace DevCard.Models
+{
+	public class Project
+	{
+		public long ID { get; set; }
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Client { get; set; }
+		public string Image { get; set; }
+
+		public Project(long id, string name, string description, string client, string image)
+		{
+			ID = id;
+			Name = name;
+			Description = description;
+			Client = client;
+			Image = image;
+		}
+	}
+}
---
+++ DevCard/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard.ViewComponents
+{
+	public class ArticlesViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+
+			var articles = new List<Article>
+			{
+				new Article(1,"آموزش ASP.Net Core MVC","کاملترین پکیج آموزش ASP.Net Core MVC به زبان فارسی","blog-post-thumb-card-1.jpg"),
+				new Article(2,"آموزش Git & GitHub","کاملترین پکیج آموزش آموزش Git & GitHub به زبان فارسی","blog-post-thumb-card-2.jpg"),
+				new Article(3,"آموزش Android Studio","کامل ترین پکیج آموزش Git & GitHub  به زبان فارسی","blog-post-thumb-card-3.jpg")
+			};
+			return View("_Articles" , articles);
+		}
+	}
+}
