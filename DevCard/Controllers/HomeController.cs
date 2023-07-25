84797244d3bd851e225a5dd9059b1cffedbbc168
neda-h <51288315+neda-h@users.noreply.github.com>
2023-07-25 11:17:57 +0430

    Contact Form Client side validation implemented

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -4,28 +4,34 @@ using System.Diagnostics;
 
 namespace DevCard.Controllers
 {
-    public class HomeController : Controller
-    {
+	public class HomeController : Controller
+	{
+		public HomeController()
+		{
+		}
 
-        public HomeController()
-        {
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Contact()
+		{
+			var model = new Contact();
+			return View(model);
+		}
 
-        public IActionResult Contact()
+        [HttpPost]
+        public JsonResult Contact(Contact form)
         {
-	        return View(); 
+            return Json(Ok());
         }
-    
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+		}
+	}
 }
---
+++ DevCard/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevCard.Models
+{
+	public class Contact
+	{
+		[Required(ErrorMessage = "این فیلد اجباری است")]
+		[MaxLength(100, ErrorMessage = "حداکثر طول نام، 100 کارکتر می باشد")]
+		[MinLength(3, ErrorMessage = "حداقل طول نام، 3 کارکتر می باشد")]
+		public String Name { get; set; }
+
+		[EmailAddress(ErrorMessage = "فرمت ایمیل وارد شده صحیح نمی باشد")]
+		[Required(ErrorMessage = "این فیلد اجباری است")]
+		public String Email { get; set; }
+
+		public String Message { get; set; }
+
+		public String Service { get; set; }
+	}
+}
